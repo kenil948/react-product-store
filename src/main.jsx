cb3952f3aef3db948d1a6c25cb29cac71fad0014
kenil948 <kenilchodavadiya21@gmail.com>
2026-05-16 16:47:34 +0530

    Fix routing issue for GitHub Pages using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Context from "./utils/Context.jsx";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <Context>
-    <BrowserRouter>
+    <HashRouter>
       <App />
        <ToastContainer />
-    </BrowserRouter>
+    </HashRouter>
   </Context>,
 );
